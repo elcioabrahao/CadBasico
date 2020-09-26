f6bfc34ec22e0542df569acd9150b01090f0fbaa
2104499700 <elcio.abrahao@viavarejo.com.br>
2020-09-25 21:13:09 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ucsist/cadbasico/LoginActivity.java
@@ -0,0 +1,28 @@
+package br.usjt.ucsist.cadbasico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.logging.Logger;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void novoCadastro(View view) {
+        Intent intent = new Intent(this, CadastroActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
