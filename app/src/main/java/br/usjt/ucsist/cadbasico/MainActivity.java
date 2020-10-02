be91736820cca70585d600b6d13a4d596e567de5
2104499700 <elcio.abrahao@viavarejo.com.br>
2020-10-02 19:38:07 -0300

    Para aluno pegar o codigo

--- app/src/main/java/br/usjt/ucsist/cadbasico/MainActivity.java
+++ app/src/main/java/br/usjt/ucsist/cadbasico/MainActivity.java
@@ -2,7 +2,10 @@ package br.usjt.ucsist.cadbasico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.editar:
+                Intent intent = new Intent(this,CadastroActivity.class);
+                startActivity(intent);
+            case R.id.sair:
+                finish();
+                return(true);
+        }
+        return(super.onOptionsItemSelected(item));
     }
 }
